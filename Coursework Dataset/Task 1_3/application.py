8f005245d8b37839d053ba43cb2da1ce0aba37fb
Sp0xF8 <spoonnosaurus@gmail.com>
2024-05-04 19:26:33 +0100

    1.3

--- Coursework Dataset/Task 1_3/application.py
+++ Coursework Dataset/Task 1_3/application.py
@@ -1,99 +1,92 @@
 import multiprocessing as mp
 
 
-manager = mp.Manager()
-name_counts = manager.dict()
 
-def search(pattern, text):
+def search(pattern, text, result_queue):
 	count = 0
-
-	for i in range(len(text)-len(pattern)+1):
-		
+	for i in range(len(text) - len(pattern) + 1):
 		if text[i] == pattern[0]:
 			found = True
 			for j in range(1, len(pattern)):
-				if text[i+j] != pattern[j]:
+				if text[i + j] != pattern[j]:
 					found = False
 					break
 			if found:
 				count += 1
-	print(count)
-	name_counts[pattern] = count
-	
+	# print(count)
+	result_queue.put((pattern, count))
 
 
 def read_files():
-
 	with open("task1_3_text.txt", "r", encoding='utf8') as f:
 		text = f.read()
-		
 
 	with open("task1_3_names.txt", "r", encoding='utf8') as f:
 		patterns = f.readlines()
 
-	#remove newline characters
+	# remove newline characters
 	patterns = [x.strip() for x in patterns]
 
 	return text, patterns
 
 
-
 def check_capacity():
-
-	##print number of processors
+	# print number of processors
 	if mp.cpu_count() < 4:
-		print("Number of processors is less than 4")
+		# print("Number of processors is less than 4")
 		n = 1
 	elif mp.cpu_count() < 8:
-		print("Number of processors is less than 8")
+		# print("Number of processors is less than 8")
 		n = 2
 	else:
-		print("Number of processors is greater than 8")
+		# print("Number of processors is greater than 8")
 		n = 4
-	
+
 	return n
 
 
 def check_patterns(patterns, text, n):
-
-
+	result_queue = mp.Queue()
 	x = 0
-	while (x < len(patterns)):
 
-		print("round start")
+	name_counts = {}
 
+	while x < len(patterns):
+		# print("round start")
 		process_list = []
-
-		if n > (len(patterns)-x):
-			stop = x + (len(patterns)-x)
+		if n > (len(patterns) - x):
+			stop = x + (len(patterns) - x)
 		else:
-			stop = x+n
-
+			stop = x + n
 
 		for i in range(x, stop):
-			print("pattern: " + patterns[i])
-
-			p = mp.Process(target=search, args=(patterns[i], text))
+			# print("pattern: " + patterns[i])
+			p = mp.Process(target=search, args=(patterns[i], text, result_queue))
 			process_list.append(p)
-		
+
 		for p in process_list:
 			p.start()
 
 		for p in process_list:
 			p.join()
 
-		
+		while not result_queue.empty():
+			pattern, count = result_queue.get()
+			name_counts[pattern] = count
+
 		x += n
+	return name_counts
 
-
-	
-#main
-if __name__ == '__main__': 
-	
+if __name__ == '__main__':
 	text, patterns = read_files()
-
 	n = check_capacity()
+	name_counts = check_patterns(patterns, text, n)
+	l_counts = list(name_counts.items())
+	str_counts =str(l_counts)
 
-	check_patterns(patterns, text, n)
+	#remove brackets
+	str_counts = str_counts.replace("[", "")
+	str_counts = str_counts.replace("]", "")
+	
 
-	print(name_counts)
+	print(str_counts)

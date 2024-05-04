171aa42bc957153290aa73d9ba587072a40c0c80
Sp0xF8 <spoonnosaurus@gmail.com>
2024-05-04 18:22:26 +0100

    close close 1.3

--- Coursework Dataset/Task 1_3/application.py
+++ Coursework Dataset/Task 1_3/application.py
@@ -1,6 +1,8 @@
 import multiprocessing as mp
-import os
 
+
+manager = mp.Manager()
+name_counts = manager.dict()
 
 def search(pattern, text):
 	count = 0
@@ -15,17 +17,83 @@ def search(pattern, text):
 					break
 			if found:
 				count += 1
-
-	return count
-
-
-with open("task1_3_text.txt", "r", encoding='utf8') as f:
-	text = f.read()
-    
+	print(count)
+	name_counts[pattern] = count
+	
 
 
+def read_files():
+
+	with open("task1_3_text.txt", "r", encoding='utf8') as f:
+		text = f.read()
+		
+
+	with open("task1_3_names.txt", "r", encoding='utf8') as f:
+		patterns = f.readlines()
+
+	#remove newline characters
+	patterns = [x.strip() for x in patterns]
+
+	return text, patterns
 
 
-hNumber = search("Ron", text)
 
-print("Number of times Harry appears in the text: ", hNumber)
+def check_capacity():
+
+	##print number of processors
+	if mp.cpu_count() < 4:
+		print("Number of processors is less than 4")
+		n = 1
+	elif mp.cpu_count() < 8:
+		print("Number of processors is less than 8")
+		n = 2
+	else:
+		print("Number of processors is greater than 8")
+		n = 4
+	
+	return n
+
+
+def check_patterns(patterns, text, n):
+
+
+	x = 0
+	while (x < len(patterns)):
+
+		print("round start")
+
+		process_list = []
+
+		if n > (len(patterns)-x):
+			stop = x + (len(patterns)-x)
+		else:
+			stop = x+n
+
+
+		for i in range(x, stop):
+			print("pattern: " + patterns[i])
+
+			p = mp.Process(target=search, args=(patterns[i], text))
+			process_list.append(p)
+		
+		for p in process_list:
+			p.start()
+
+		for p in process_list:
+			p.join()
+
+		
+		x += n
+
+
+	
+#main
+if __name__ == '__main__': 
+	
+	text, patterns = read_files()
+
+	n = check_capacity()
+
+	check_patterns(patterns, text, n)
+
+	print(name_counts)

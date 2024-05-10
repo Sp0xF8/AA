5ba0377347315f5f85918b66f5d41cc655d8b26e
Sp0xF8 <spoonnosaurus@gmail.com>
2024-05-10 21:33:05 +0100

    1.3 100%

--- Coursework Dataset/Task 1_3/application.py
+++ Coursework Dataset/Task 1_3/application.py
@@ -6,7 +6,6 @@ def get_Ram():
 	ram = proc.readlines()
 	proc.close()
 
-	# print(ram[:])
 
 	## for each line in the output
 	total_ram = 0
@@ -22,7 +21,6 @@ def get_Ram():
 
 	## convert to GB from bytes
 	total_ram = total_ram / (1024**3)
-	# print(total_ram)
 
 	return total_ram
 
@@ -30,27 +28,26 @@ def check_capacity():
 	# print number of processors
 	if mp.cpu_count() < 4:
 		# print("Number of processors is less than 4")
-		n = 1
+		n = 2
 	elif mp.cpu_count() < 8:
 		# print("Number of processors is less than 8")
-		n = 2
+		n = 4
 	else:
 		# print("Number of processors is greater than 8")
-		n = 4
+		n = 6
+	
+	return n# 4
 
-	return n
 
-
-def search_size(path):
+def get_params(path, verbose, force=None):
 
 	ram = get_Ram()
 	n = check_capacity()
 
+	
+
 	size = 0
-	print("Total RAM: ", ram)
 	available_ram = ram * 0.4
-	print("Aproximated RAM: ", available_ram)
-	print("Number of processors to be used: ", n)
 
 	with open(path, "r", encoding='utf8') as f:
 		text = f.read()
@@ -60,15 +57,21 @@ def search_size(path):
 	size = size / (1024**3)
 	length = len(text)
 
-	print("Size of file: ", size)
+	if verbose:
+		print("Total RAM: ", ram)
+		print("Aproximated RAM: ", available_ram)
+		print("Number of processors to be used: ", n)
+		print("Size of file: ", size)
+		print("Length of file: ", length)
 	div = 1
 
-	if True: #size * n > available_ram /2
+	if (size * n > available_ram /2) or (force == 1 or force == 2): 
 		div = n
+		
 		size = size / n
 		length = int(length / n)
 
-		if True: #div * n > available_ram /2
+		if (size * n > available_ram /2) or (force == 2):
 			div = n*n
 			size = size / n 
 			length = int(length / n)
@@ -77,11 +80,11 @@ def search_size(path):
 		print("Size of file * number of processors is less than available RAM")
 		print("The file will not be split into smaller parts")
 
-
-	print("Number of parts: ", div)
-	print("Size of each part: ", size)
-	print("Length of each part: ", length)
-
+	if verbose:
+		print("Number of parts: ", div)
+		print("Size of each part: ", size)
+		print("Length of each part: ", length)
+		input("Press Enter to continue...")
 
 	return length, text, div
 
@@ -93,8 +96,6 @@ def get_patterns(path):
 
 	print(patterns)
 	return patterns
-
-
 
 def search(pattern, text, result_queue):
 	count = 0
@@ -108,16 +109,16 @@ def search(pattern, text, result_queue):
 					break
 			if found:
 				count += 1
-	# print(count)
 	result_queue.put((pattern, count))
 
+def check_patterns(text_path, patterns_path, verbose=False, force=None):
 
+	n = check_capacity()
 
-def check_patterns(text_path, patterns_path, n):
 	result_queue = mp.Queue()
 	name_counts = {}
 
-	length, text, div = search_size(text_path)
+	length, text, div = get_params(text_path, verbose, force)
 	patterns = get_patterns(patterns_path)
 
 
@@ -131,7 +132,8 @@ def check_patterns(text_path, patterns_path, n):
 			pattern = patterns[x]
 			pattern_length = len(pattern)
 
-			print("Pattern: ", pattern)
+			if verbose:
+				print("Pattern: ", pattern)
 			while pos < len(text):
 				processes = []
 				if end == len(text):
@@ -142,22 +144,23 @@ def check_patterns(text_path, patterns_path, n):
 
 				for i in range(n):
 					loop += 1
-					start = pos - (pattern_length - 1)
+					start = pos - int(pattern_length/2)
 					if start < 0:
 						start = 0
 
-					end = pos + length + (pattern_length - 1)
+					end = pos + length + int(pattern_length/2)
 					if end > len(text):
 						end = len(text)
-					
-					# print("Start: ", start)
-					# print("End: ", end)
+					if verbose:
+						# print("Start: ", start)
+						# print("End: ", end)
+						print("Qeueing: ", pattern, " Part: ", loop)
 					process = mp.Process(target=search, args=(pattern, text[start:end], result_queue))
 					processes.append(process)
-					print("Qeueing: ", pattern, " Part: ", loop)
 
 					if end == len(text):
-						# print("End of file")
+						if verbose:
+							print("End of text reached")
 						break
 
 					pos += length
@@ -174,9 +177,11 @@ def check_patterns(text_path, patterns_path, n):
 					if pattern not in name_counts:
 						name_counts[pattern] = 0
 					name_counts[pattern] += count
-					print("Received: ", pattern, " Count: ", count, " Part: ", l_start)
+					if verbose:
+						print("Received: ", pattern, " Count: ", count, " Part: ", l_start)
 					l_start += 1
-				print("Total for: ", pattern, " is: ", name_counts[pattern])
+				if verbose:
+					print("Total for: ", pattern, " is: ", name_counts[pattern])
 			x += 1
 
 		return name_counts
@@ -214,7 +219,7 @@ if __name__ == '__main__':
 	# search_base_length, text, div = search_size("task1_3_text.txt")
 	# print("Base Length: ", search_base_length)
 
-	name_counts = check_patterns("task1_3_text.txt", "task1_3_names.txt", 4)
+	name_counts = check_patterns("task1_3_text.txt", "task1_3_names.txt", verbose=True, force=2)
 	l_counts = list(name_counts.items())
 	str_counts =str(l_counts)
 
@@ -229,8 +234,6 @@ if __name__ == '__main__':
 
 
 #output
-##('Harry', 3991), ('Ron', 1185), ('Hermione', 1217), ('Hagrid', 168), ('Dumbledore', 588), ('Draco', 62), ('McGonagall', 70), ('Snape', 292), ('Gilderoy', 0), ('Ginny', 122), ('Malfoy', 89), ('Vernon', 50), ('Arthur', 20), ('Molly', 11), ('Sirius', 69), ('Remus', 21), ('Peter', 2), ('Neville', 89), ('Fred', 94), ('George', 77), ('Moody', 30), ('Cho', 13), ('Voldemort', 446), ('Bellatrix', 98), ('Luna', 140)
-##('Harry', 3991), ('Ron', 1185), ('Hermione', 1217), ('Hagrid', 168), ('Dumbledore', 588), ('Draco', 62), ('McGonagall', 70), ('Snape', 292), ('Gilderoy', 0), ('Ginny', 122), ('Malfoy', 89), ('Vernon', 50), ('Arthur', 20), ('Molly', 11), ('Sirius', 69), ('Remus', 21), ('Peter', 2), ('Neville', 89), ('Fred', 94), ('George', 77), ('Moody', 30), ('Cho', 13), ('Voldemort', 446), ('Bellatrix', 98), ('Luna', 140)
 ##('Harry', 3991), ('Ron', 1185), ('Hermione', 1217), ('Hagrid', 168), ('Dumbledore', 588), ('Draco', 62), ('McGonagall', 70), ('Snape', 292), ('Gilderoy', 0), ('Ginny', 122), ('Malfoy', 89), ('Vernon', 50), ('Arthur', 20), ('Molly', 11), ('Sirius', 69), ('Remus', 21), ('Peter', 2), ('Neville', 89), ('Fred', 94), ('George', 77), ('Moody', 30), ('Cho', 13), ('Voldemort', 446), ('Bellatrix', 98), ('Luna', 140)
 
 ##expected output
